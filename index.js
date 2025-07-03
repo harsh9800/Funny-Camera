b2999aff44015ed9fc2efc602d6f84a209632af1
harsh9800 <148126866+harsh9800@users.noreply.github.com>
2025-07-03 13:36:00 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,4 @@
+function namee(event){
+    document.querySelector(".myPhone").style.backgroundImage = "url('WhatsApp%20Image%202025-07-03%20at%2012.23.35.jpeg')";
+}
+document.querySelector(".front-camera").addEventListener("click",namee);
